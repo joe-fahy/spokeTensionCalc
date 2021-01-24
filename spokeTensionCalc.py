ccc8939425cb70ec44d0a29354c4afb641ba0d60
Joe Fahy <40170659+joe-fahy@users.noreply.github.com>
2021-01-24 13:45:10 +0000

    Updated formatting of output.

--- spokeTensionCalc.py
+++ spokeTensionCalc.py
@@ -2,24 +2,15 @@ import math
 
 frequency = 440.0
 
-lengthCm = 26
-
-diaCm = 0.2
-
-densityGmCm = 7.8
-
-tensionIn = math.pi * (frequency**2) * (lengthCm**2) * (diaCm**2) * densityGmCm
-
-#print(tensionIn)
 
 
 #f^2 * 4mL = T
 spokeDia = 0.002
 spokeLen = 0.245
 volOfSpoke = spokeLen * ((spokeDia/2)**2) * math.pi
-print("volOfSpoke = {}".format(volOfSpoke))
+print("Volume Of Spoke = {} Metres Cubed".format(volOfSpoke))
 massSpoke = volOfSpoke * 7480.0
-print("massSpoke = {}".format(massSpoke))
+print("Mass of Spoke = {} Kilograms".format(massSpoke))
 #massOfSpoke = (math.pi * (0.02**2) * 0.26)
 #print(massOfSpoke)
 #6.2g in kg is 0.0062
@@ -28,4 +19,4 @@ newTensionNewtons = (frequency**2) * (4 * massSpoke * spokeLen)
 
 #print("New tension in newtons =  {}".format(newTensionNewons))
 
-print(newTensionNewtons)
+print("Tension = {} Newtons".format(newTensionNewtons))
